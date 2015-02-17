4d39d6e0d336f3571283fcc540c6ea8f455c0105
Vutov <VutovSpas@gmail.com>
2015-02-18 00:39:23 +0200

    css on master page done

--- DevNest/DevNest/MasterPage.Master.designer.cs
+++ DevNest/DevNest/MasterPage.Master.designer.cs
@@ -31,13 +31,13 @@ namespace DevNest {
         protected global::System.Web.UI.HtmlControls.HtmlForm MasterForm;
         
         /// <summary>
-        /// WelcomeUser control.
+        /// Label2 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Label WelcomeUser;
+        protected global::System.Web.UI.WebControls.Label Label2;
         
         /// <summary>
         /// ContentMaster control.
